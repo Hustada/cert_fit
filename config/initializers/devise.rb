3ede2d1a7263d9716d0f8d83de1e74a682635327
Mark Hustad <hustada80@gmail.com>
2017-09-26 10:20:45 -0500

    edit google auth live config

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -256,10 +256,7 @@ Devise.setup do |config|
     config.omniauth :facebook, ENV["facebook_live_app_id"], ENV["facebook_live_app_secret"],
     scope: 'email', info_fields: 'email,name'
 
-    config.omniauth :google_oauth2, ENV["google_live_app_id"], ENV["google_live_app_secret"],
-    { scope: 'email',
-      prompt: 'select_account',
-      access_type: 'online'
+    config.omniauth :google_oauth2, ENV["google_live_app_id"], ENV["google_live_app_secret"],{ 
     }
 
   elsif Rails.env.development?
